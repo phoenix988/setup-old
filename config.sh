c5447c16b3569e472e024c26ddae8920ef31a8d8
phoenix988 <karlfredin@gmail.com>
2022-07-18 12:34:28 +0300

    updating config.sh and files and adding some more configs

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-    
+   check_os=$(cat /etc/os-release | awk -F = '/^NAME/ {print $2}' | sed 's/"//g' | awk '{print $1}') 
+   
    until [ "$install_docker" = "y" -o "$install_docker" = "n"  ]
    do
    read -p "do you want to install docker? [y/n]: " install_docker 
@@ -9,15 +10,24 @@
 
    done
    
-   exit
+   if [ $check_os = "Arch" ] ; then
+   
+   
+      until [ "$install_xorg" = "y" -o "$install_xorg" = "n"  ]
+   do
+    
+      read -p "Do you want to install Xorg? [y/n]: " install_xorg 
+   
+        printf "\nPlease type y or n\n"
+   
+     done 
+   fi 
 
 
+exit
 
 
-
-
-
-    #Will exit the script if you are root since I dont recommend running this as root
+   #Will exit the script if you are root since I dont recommend running this as root
     [ $UID = "0" ] && printf "Don't run this script as root ..... aborting" && exit
    
     if [ -d $HOME/setup ] ; then
@@ -51,35 +61,44 @@
     fi
     
     #Clones the git hub repo so we can access all the files we need
+    printf "\nCloning My git repo to get all my config files ready"
     git clone https://github.com/phoenix988/setup $HOME/setup > /dev/null 
 
     [ $? != "0" ] && printf "\n cloning repo failed......aborting" && exit
    
-    printf "\nCloning My git repo to get all my config files ready"
 
     fi 
 
-   #Preparing oh my zsh so it will be installed
     
-      printf "\nChecks if zsh is installed\n" 
+        
+   #This makes sure oh my zsh and zsh will be configured 
+   #and setup correctly using my config files
 
-   if [ -d $HOME/.config/oh-my-zsh ] ; then     
+      printf "\nChecks if zsh is installed\n" 
        
-        printf "\nOh my Zsh is Installed\n"
-        sleep 2
-   else
-      
    if [ -e /usr/bin/zsh ] ; then
        
-      printf "\nInstalls zsh only if its needed\n"
+      printf "\nzsh already Installed\n"
       sleep 2
-[ -d /etc/dnf ] && sudo dnf install -y zsh     
-[ -d /etc/apt ] && sudo apt install -y zsh     
-[ -e /etc/pacman.conf ] && sudo pacman -S zsh  
+   else 
+
+  [ -d /etc/dnf ] && sudo dnf install -y zsh    &> /dev/null 
+  [ -d /etc/apt ] && sudo apt install -y zsh    &> /dev/null 
+  [ -e /etc/pacman.conf ] && sudo pacman -S zsh &> /dev/null 
 
   fi
 
-       sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        printf "\nChecks if oh-my-zsh is installed\n" 
+
+        if [ -d $HOME/.config/oh-my-zsh ] ; then     
+          
+           printf "\nOh my Zsh is Installed\n"
+           sleep 2
+      
+        else
+
+        #Installs oh my zsh
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
          
         [ -d $HOME/.config ] || mkdir $HOME/.config
         sudo cp -r $HOME/.oh-my-zsh $HOME/.config/oh-my-zsh
@@ -87,14 +106,31 @@
         [ -e $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh
        
         sudo chown karl:karl $HOME/.config/oh-my-zsh
-      
+        #Install some zsh plugins     
         git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/oh-my-zsh/zsh-autosuggestions
 
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting /home/karl/.config/oh-my-zsh/zsh-syntax-highlighting
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/oh-my-zsh/zsh-syntax-highlighting
 
 
 
    fi
+        #This will change the default shell to zsh
+        cat /usr/bin/zsh &> /dev/null 
+
+        printf "\n Changing Default shell to zsh if needed\n"
+        [ $? = "0" ] && usermod=$(sudo usermod -s /bin/zsh $USER | awk '{print $3}') || printf "\n ZSH is not installed wont change shell\n"
+
+        #Clones oh my tmux
+        [ -d $HOME/.tmux ] || git clone https://github.com/gpakosz/.tmux.git  $HOME/.tmux
+
+        #Only links .tmux.conf if it doesnt exist already
+        printf "\nLinkink $HOME/.tmux/.tmux.conf if needed\n"
+        
+        sleep 2
+      
+        [ -e $HOME/.tmux.conf ] || ln -s $HOME/.tmux/.tmux.conf $HOME/.tmux.conf 
+
+
 
     #adding some long paths to variables so it will be easier to use
     fstab="/home/karl/setup/files/fstab"
@@ -105,8 +141,7 @@
     
    
     #config path
-
-    config="/home/karl/setup/config"
+    config="$HOME/setup/config"
 
     #config_files
     tmuxconflocal="$config/.tmux.conf.local"
@@ -121,6 +156,14 @@
     rofi="$config/rofi"
     starship="$config/starship.toml"
 
+    
+
+    #This will create .dmenu and .scripts in $HOME if they dont exist
+    printf "\nCreating script folders in $HOME if they doesnt exist already\n"
+    sleep 2
+    [ -d $HOME/.scripts ] || mkdir $HOME/.scripts
+    [ -d $HOME/.dmenu ] || mkdir $HOME/.dmenu
+
     #Edits the fstab if needed to add my script folder
     #Temporarily change ownership on fstab
     #And checks your fstab to see if you already have the correct entries
@@ -130,7 +173,11 @@
     check_dmenu=$(cat /etc/fstab | awk '/.dmenu/ {print $1}')
     ping 192.168.1.10 -c 1 &> /dev/null 
     
-    printf "\nModifying your fstab if needed\n"  
+    
+    if [ $? = "0" ] ; then 
+           
+          
+    printf "\nPing succeded , Modifying your fstab if needed\n"  
     sleep 2
     
     #Modifying the fstab but this is temporaily and it will change back to default permissions
@@ -144,20 +191,16 @@
     
     #Change the ownership back to root on fstab 
     sudo chown root:root /etc/fstab
-
-
-    
-    #This will create .dmenu and .scripts in $HOME if they dont exist
-    printf "\nCreating script folders in $HOME if they doesnt exist already\n"
-    sleep 2
-    [ -d $HOME/.scripts ] || mkdir $HOME/.scripts
-    [ -d $HOME/.dmenu ] || mkdir $HOME/.dmenu
-
     
     #mounting everything from fstab
-   # printf "\nRunning mount -a to mount all entris from the fstab\n"
-   # sudo mount -a
+    printf "\nRunning mount -a to mount all entris from the fstab\n"
+    sudo mount -a
 
+    else 
+        
+    printf "\nPing failed Nothing will be done\n"  
+    
+    fi
     
     #Checks what package manager you have and then install some packages
     #also it will do some other things based on what distro you have
@@ -203,7 +246,7 @@
        #just add the package name to that list if you want 
        #this script to install it for you
        printf "\nInstalling dnf packages from my package list if needed\n"
-       sudo dnf install $(cat $dnf) -y &> /dev/null 2> $HOME/.dnf.error
+       sudo dnf install $(cat $dnf) -y > /dev/null 2> $HOME/.dnf.error
        
        [ -e $HOME/.dnf.error ] && errordnf=$(cat $HOME/.dnf.error)
        [ -z $errordnf ] || printf "\n You got some error installing packages here is the log \n\n $error-dnf"
@@ -212,6 +255,8 @@
        #This will install docker if you are running fedora
        #it will check for the distro id and if the id is fedora
        #this script will install docker
+       if [ $install_docker = "y" ] ; then
+
        [ "$fedora" = "fedora" ] && 
        printf "\n I see you're using fedora ... so adding docker repo and installs it....\n"
        sudo dnf config-manager \
@@ -219,7 +264,8 @@
        https://download.docker.com/linux/fedora/docker-ce.repo > /dev/null && \
        sudo dnf install docker-ce docker-ce-cli containerd.io -y &> /dev/null && \
        sudo systemctl enable --now docker &> /dev/null && sudo usermod -aG docker $USER &> /dev/null
-
+       
+       fi 
        #We dont ever wanna use nano
        #so lets remove it if its installed
        printf "\n Removing nano if its installed\n"
@@ -235,17 +281,15 @@
     #This is for pacman or arch based distors
     if [ -e /etc/pacman.conf ] ; then
        
+       printf "\n Adding Pacman Repos if needed\n"
        #This will check if you already have andonties repo in your pacman.conf 
        check_pacman=$(cat /etc/pacman.conf | awk '/andontie-aur/' | \
        sed -e 's/\[//g' -e 's/\]//g')
-   
        #modifying pacman.conf if needed 
        #Temporarily change ownership on pacman.conf
        sudo chown $USER:$USER /etc/pacman.conf
-       
        #If check_pacman is empty then it will add the repos to pacman.conf
        [ -z $check_pacman ] && cat $pacman_conf >> /etc/pacman.conf
-
        #Change the ownsership back to root on pacman.conf
        sudo chown root:root /etc/pacman.conf
        
@@ -253,40 +297,56 @@
        #just add the package name to that list if you want 
        #this script to install it for you
        printf "\nInstalling pacman packages from my package list if needed\n"
-       sudo pacman -Sy $(cat $pacman) --needed --noconfirm &> /dev/null
+       sudo pacman -Sy $(cat $pacman) --needed --noconfirm > /dev/null 2> $HOME/.pacman.error
       
        
-       #Installs Docker if you want to
-       #Also asks you if you want portainer agent up and running using docker
+       #Installs Docker if you said yes 
+      if [ $install_docker = "y" ] ; then 
 
-       #Installs pfetch
-       printf "\nInstalling pfetch if its no installed already using paru\n" 
-       paru -S pfetch --needed --noconfirm &> /dev/null
-       paru -S autofs --needed --noconfirm &> /dev/null
-      
-       #This will deactivate logins for root
-       check_root_if_activated=$(grep "root" /etc/passwd | awk -F : '{print $NF}' | awk -F / '{print $NF}')
-       [ $check_root_if_activated != "nologin" ] && usermod_root=$(sudo usermod -s /usr/bin/nologin root | awk '{print $3}')       
-     
+       printf "\nInstalling Docker\n"
+       sudo pacman -S docker &> /dev/null
 
        #adds the user to the docker group
        sudo usermod -aG docker $USER &> /dev/null
 
        #this will start the docker daemon if its not running
        sudo systemctl enable --now docker &> /dev/null
-    fi
 
+       fi
+   
+       #installs xorg if you said yes 
+       if [ $install_xorg  = "y" ] ; then
+          printf "\nInstalls xorg\n"
+          sudo pacman -S xorg --needed --noconfirm &> /dev/null
+       fi
+       #Installs pfetch
+       printf "\nInstalling pfetch and autofs if its no installed already using paru\n" 
+       paru -S pfetch --needed --noconfirm &> /dev/null
+       paru -S autofs --needed --noconfirm &> /dev/null
+       paru -S mutt-wizard --needed --noconfirm &> /dev/null
+      
+       #This will deactivate logins for root
+
+       
+       printf "\nDeactivating root account for security reasons , if its needed\n"
+       check_root_if_activated=$(grep "root" /etc/passwd | awk -F : '{print $NF}' | awk -F / '{print $NF}')
+       [ $check_root_if_activated != "nologin" ] && usermod_root=$(sudo usermod -s /usr/bin/nologin root | awk '{print $3}')       
+     
+
+      fi
+      
+      #removes oh my zsh if its for some reason exist in the wrong directory
       [ -e $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh
-    #Installs the starship prompt if its not installed   
-    #This will print the version of starship Installed 
-    #But if it's not installed this script will go ahead and install it
-    printf "\n Checks if starship is installed .. and installs it if its not\n"
-    sleep 2
-    starship -V &> /dev/null
+      #Installs the starship prompt if its not installed   
+      #This will print the version of starship Installed 
+      #But if it's not installed this script will go ahead and install it
+      printf "\n Checks if starship is installed .. and installs it if its not\n"
+      sleep 2
+      starship -V &> /dev/null
 
-    [ $? != "0" ] && wget https://starship.rs/install.sh &> /dev/null 
-    
-    [ -e $HOME/install.sh ] && sudo chmod 755 ./install.sh && sudo ./install.sh --yes > /dev/null && rm $HOME/install.sh 
+      [ $? != "0" ] && wget https://starship.rs/install.sh &> /dev/null 
+      
+      [ -e $HOME/install.sh ] && sudo chmod 755 ./install.sh && sudo ./install.sh --yes > /dev/null && rm $HOME/install.sh 
     
 
    #Copying all my config files from the git repo I cloned to your
@@ -317,22 +377,7 @@
     cp -r $starship $HOME/.config/
 
 
-    #Clones oh my tmux
-    [ -d $HOME/.tmux ] || git clone https://github.com/gpakosz/.tmux.git 
-
-   #Only creates .tmux.conf if it doesnt exist already
-    if [ -d $HOME/.tmux ] ; then
-    cat $HOME/.tmux.conf &> /dev/null
-    [ $? != "0" ] && ln $HOME/.tmux/.tmux.conf $HOME/.tmux.conf && \
-    printf "\nLinking $HOME/.tmux.conf\n"
-    
-    sleep 2
-    
-    else 
-       printf "\n$HOME/.tmux doesnt exist.... aborts this step"
-    sleep 2
-    fi
-    #only link neovim to vim if neovim is installed 
+       #only link neovim to vim if neovim is installed 
     if [ -e /usr/bin/nvim ] ; then
 
     #This will link vim to neovim
@@ -348,29 +393,24 @@
     fi
    
     
-    #This will change the default shell to zsh
-    cat /usr/bin/zsh &> /dev/null 
-
-    printf "\n Changing Default shell to zsh\n"
-    [ $? = "0" ] && usermod=$(sudo usermod -s /bin/zsh $USER | awk '{print $3}') || printf "\n ZSH is not installed wont change shell\n"
-
-    
-     sleep 2
+         sleep 2
          
 
 
     #This will create .config folder for root
-    printf "\n\n Creates config folder for root\n"
-    sudo mkdir /root/.config &> /dev/null
+    printf "\n\n Creates config folder for root if it doesnt exist\n"
+    [ -d /root/.config ] && sudo mkdir /root/.config &> /dev/null
 
     #This will link the neovim config to the root USER
     #so neovim will have the same config even if you edit something as root
-    [ -d $HOME/.config/nvim ] && sudo ln -s $HOME/.config/nvim /root/.config/nvim &> /dev/null && \
        printf "\nLinking neovim config to root so it uses the same neovim config as the user\n"
+    
+       [ -d $HOME/.config/nvim ] && sudo ln -s $HOME/.config/nvim /root/.config/nvim &> /dev/null && \
        sleep 2
 
     #This will install portainer agent on the host
-    #only if docker is installed on the system
+    #only if choose to install docker on the system
+    if [ $install_docker = "y" ] ; then
     docker -v &> /dev/null 
 
     if [ $? = "0" ] ; then
@@ -382,4 +422,6 @@
     -v /var/run/docker.sock:/var/run/docker.sock  \
     -v portainer_data_agent:/var/lib/docker/volumes portainer/agent &> /dev/null 
     
+    fi
+
     fi
