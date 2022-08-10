88909e55c54404331dda6997593b798465e2e4ed
Karl Fredin <karlfredin@gmail.com>
2022-08-10 19:05:09 +0300

    updating apt section of the script

--- config.sh
+++ config.sh
@@ -684,8 +684,8 @@ if [ -d /etc/apt ] ; then
          echo "############################################################"
          sudo apt install $(cat $apt) -y 2> $HOME/.apt.error
         
-         wget https://github.com/Peltoche/lsd/releases/download/0.22.0/lsd-musl_0.22.0_amd64.deb && sudo dpkg -i ./lsd-musl_0.22.0_amd64
-         rm -rf lsd-musl_0.22.0_amd64
+         wget https://github.com/Peltoche/lsd/releases/download/0.22.0/lsd-musl_0.22.0_amd64.deb && sudo dpkg -i ./lsd-musl_0.22.0_amd64.deb
+         rm -rf lsd-musl_0.22.0_amd64.deb
 
 
          #This will check if nano is installed or not 
